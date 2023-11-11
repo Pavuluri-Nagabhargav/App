4dc31f4ac5329d7d9661c43cc2a823d1bda66ac1
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 11:50:01 -0600

    Update doclink.py

--- doclink.py
+++ doclink.py
@@ -70,6 +70,15 @@ def main():
 
     if not st.session_state.get('logged_in', False):
         st.warning("Please log in to access the patient reports.")
+        st.header("Overview")
+        st.write("""
+        In our mission to advance respiratory healthcare, we are developing a product to facilitate early diagnosis of Chronic Obstructive Pulmonary Disease (COPD). 
+        Our primary focus is on collaborating with rural clinics, empowering them with a subscription-based service. These clinics, often lacking respiratory specialists, can leverage our KIOSK system to conduct initial patient surveys. The gathered data is securely stored in a dedicated physician's account, provided as part of the subscription.
+
+        The process involves analyzing patient symptoms to offer timely recommendations for advanced tests. This proactive approach enables patients to undergo necessary examinations without enduring prolonged waits for specialty appointments. Once recommended, patients can upload their test reports through our portal. These reports serve as inputs for our advanced machine learning and AI models, predicting the likelihood of COPD and its stages.
+
+        Our ultimate goal is to provide early intervention, saving patients valuable time and ensuring timely treatment before the disease progresses to more severe stages.
+        """)
         return
 
     # Display patient reports folder
