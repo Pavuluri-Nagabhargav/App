4b8db3f015291ed9686df3e45b5d24c6128a8ca5
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 12:25:38 -0600

    Update doclink.py

--- doclink.py
+++ doclink.py
@@ -1,20 +1,20 @@
 import streamlit as st
-from typing import Dict, Any
+import time  # Import the time module for the sleep function
 
 # Mock database to store patient details
-patients_db: Dict[str, Dict[str, Any]] = {
+patients_db = {
     "0001": {
         "full_name": "Sai",
         "phone_number": "8888888888",
         "shortness_of_breath": True,
-        "physical_activity": False,
+        "physical_activity": True,
         "persistent_cough": True,
         "cough_type": "Productive",
         "mucus_production": True,
         "mucus_color": "Yellow",
         "mucus_consistency": "Thick",
         "H_issue": True,
-        "Smoke": False,
+        "Smoke": True,
         # ... (include other survey details)
     },
     "0002": {
@@ -24,18 +24,16 @@ patients_db: Dict[str, Dict[str, Any]] = {
         "physical_activity": False,
         "persistent_cough": True,
         "cough_type": "",
-        "mucus_production": True,
+        "mucus_production": False,
         "mucus_color": "",
         "mucus_consistency": "",
         "H_issue": False,
-        "Smoke": False,
-        
-
+        "Smoke": True,
         # ... (include other survey details)
     },
 }
 
-def recommendation(patient_data: Dict[str, Any]) -> str:
+def recommendation(patient_data):
     # Your recommendation logic based on survey answers
     # This is a placeholder, adjust it according to your requirements
     yes_count = sum(value for key, value in patient_data.items() if isinstance(value, bool) and value)
@@ -79,51 +77,36 @@ def main():
 
     if not st.session_state.get('logged_in', False):
         st.warning("Please log in to access the patient reports.")
-        st.header("Overview")
-        st.write("""
-        Lung Assist System is committed to transforming respiratory healthcare, In our mission to advance respiratory healthcare, we are developing a product to facilitate early diagnosis of Chronic Obstructive Pulmonary Disease (COPD). 
-        Our primary focus is on collaborating with rural clinics, empowering them with a subscription-based service. These clinics, often lacking respiratory specialists, can leverage our KIOSK system to conduct initial patient surveys. The gathered data is securely stored in a dedicated physician's account, provided as part of the subscription.
-
-        The process involves analyzing patient symptoms to offer timely recommendations for advanced tests. This proactive approach enables patients to undergo necessary examinations without enduring prolonged waits for specialty appointments. Once recommended, patients can upload their test reports through our portal. These reports serve as inputs for our advanced machine learning and AI models, predicting the likelihood of COPD and its stages.
-
-        Our ultimate goal is to provide early intervention, saving patients valuable time and ensuring timely treatment before the disease progresses to more severe stages.
-        """)
         return
 
     # Display patient reports folder
     st.header("Patient Reports Folder")
     selected_id = st.selectbox("Please select a patient ID", [""] + list(patients_db.keys()))
-    if selected_id:
-        if selected_id != "":
-            # Retrieve and display detailed survey data based on the selected patient ID
-            detailed_data = patients_db[selected_id]
-            st.subheader(f"Survey Report for Patient ID: {selected_id}")
-            st.write(f"Name: {detailed_data.get('full_name', 'N/A')}")
-            st.write(f"Phone Number: {detailed_data.get('phone_number', 'N/A')}")
+    if selected_id and selected_id != "":
+        # Retrieve and display detailed survey data based on the selected patient ID
+        detailed_data = patients_db[selected_id]
+        st.subheader(f"Survey Report for Patient ID: {selected_id}")
+        st.write(f"Name: {detailed_data.get('full_name', 'N/A')}")
+        st.write(f"Phone Number: {detailed_data.get('phone_number', 'N/A')}")
 
-            # Display other survey details...
-            st.header("Symptom-Related Questions:")
-            st.write(f"Shortness of Breath: {detailed_data.get('shortness_of_breath', 'N/A')}")
-            st.write(f"Physical Activity: {detailed_data.get('physical_activity', 'N/A')}")
-            st.write(f"Persistent Cough: {detailed_data.get('persistent_cough', 'N/A')}")
-            st.write(f"Cough Type: {detailed_data.get('cough_type', 'N/A')}")
-            st.write(f"Mucus Production: {detailed_data.get('mucus_production', 'N/A')}")
-            st.write(f"Mucus Color: {detailed_data.get('mucus_color', 'N/A')}")
-            st.write(f"Mucus Consistency: {detailed_data.get('mucus_consistency', 'N/A')}")
-            st.write(f"Hereditary Issues: {detailed_data.get('H_issue', 'N/A')}")
-            st.write(f"Smoking Habit: {detailed_data.get('Smoke', 'N/A')}")
+        # Display other survey details...
+        st.header("Symptom-Related Questions:")
+        st.write(f"Shortness of Breath: {detailed_data.get('shortness_of_breath', 'N/A')}")
+        st.write(f"Physical Activity: {detailed_data.get('physical_activity', 'N/A')}")
+        st.write(f"Persistent Cough: {detailed_data.get('persistent_cough', 'N/A')}")
+        st.write(f"Cough Type: {detailed_data.get('cough_type', 'N/A')}")
+        st.write(f"Mucus Production: {detailed_data.get('mucus_production', 'N/A')}")
+        st.write(f"Mucus Color: {detailed_data.get('mucus_color', 'N/A')}")
+        st.write(f"Mucus Consistency: {detailed_data.get('mucus_consistency', 'N/A')}")
+        st.write(f"Hereditary Issues: {detailed_data.get('H_issue', 'N/A')}")
+        st.write(f"Smoking Habit: {detailed_data.get('Smoke', 'N/A')}")
 
-
-            # ... (include other survey details)
-
-            # Provide recommendation using st.expander
-            with st.expander("Recommendation Details"):
-                recommendation_text = recommendation(detailed_data)
-                st.write(recommendation_text)
-                if recommendation_text == "Need further respiratory tests":
-                    upload_button = st.button("Upload Test Results")
-                    if upload_button:
-                        upload_test_results()
+        # Provide recommendation using st.expander
+        recommendation_text = recommendation(detailed_data)
+        st.subheader("Recommendation Details")
+        st.write(recommendation_text)
+        if recommendation_text == "Need further respiratory tests":
+            upload_test_results()
 
 if __name__ == "__main__":
     main()
