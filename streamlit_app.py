7df4e410bffb3f347a0fae370a90c67a9601eeca
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 08:54:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,24 @@
 import streamlit as st
 
+def welcome_screen():
+    st.title("Welcome to Lung Assist Respiratory Survey")
+    st.write("This survey is designed to gather information about your respiratory health.")
+
+    if st.button("Next"):
+        survey_container = st.empty()
+        survey_page(survey_container)
+
+def survey_page(container):
+    container.title("Respiratory Health Survey")
+
+    # Your survey questions and form go here...
+
+    if st.button("Submit Survey"):
+        container.empty()  # Clear the container
+        st.success("Your respiratory survey has been successfully submitted. Thank you!")
+
 def main():
-    st.title("Screen Clearing Example")
-
-    # User selects an action
-    action = st.radio("Select an action:", ["Show Content 1", "Show Content 2"])
-
-    # Display content based on user selection
-    if action == "Show Content 1":
-        show_content_1()
-    elif action == "Show Content 2":
-        show_content_2()
-
-def show_content_1():
-    st.header("Content 1")
-    st.write("This is the first content.")
-
-def show_content_2():
-    st.header("Content 2")
-    st.write("This is the second content.")
+    welcome_screen()
 
 if __name__ == "__main__":
     main()
