c17cbd941d973238d2abe5341ece0de202bfa07d
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 00:38:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from PIL import Image
+import requests
+from io import BytesIO
 
 def main():
     st.title("Respiratory Health Survey")
 
-    # Add transparent image of lungs as background
-    image_path = "https://images.theconversation.com/files/485402/original/file-20220919-12-mtp5y0.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C2944%2C2036&q=45&auto=format&w=926&fit=clip"  # Replace with the actual path to the transparent lungs image
-    image = Image.open(image_path)
+    # Load background image from URL
+    image_url = "https://images.theconversation.com/files/485402/original/file-20220919-12-mtp5y0.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C2944%2C2036&q=45&auto=format&w=926&fit=clip"
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
     st.image(image, use_column_width=True, output_format='PNG')
 
     # Full Name and Phone Number
