c9efd00e8526f38d50513fe62b204a6fa75bf143
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 02:40:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,39 +119,7 @@ def main():
 
         secondhand_smoke = st.checkbox("Are you exposed to secondhand smoke?")
 
-    # Occupational and Environmental Exposure
-    st.header("Occupational and Environmental Exposure")
-    workplace_exposure = st.checkbox("Are you exposed to dust, chemicals, fumes, or other irritants at your workplace?")
-    air_pollution = st.checkbox("Do you live in an area with high air pollution?")
-
-    # Family History
-    st.header("Family History")
-    family_history = st.checkbox("Is there a history of COPD or other chronic respiratory diseases in your family?")
-
-        # Lifestyle and Daily Impact
-    st.header("Lifestyle and Daily Impact")
-    daily_activities = st.text_area("How do your symptoms affect your daily activities and quality of life?")
-    regular_activities = st.checkbox("Are you able to perform regular activities without becoming short of breath?")
-    weight_loss_weakness = st.checkbox("Have you noticed any weight loss or muscle weakness?")
-
-    # Previous Health Issues
-    st.header("Previous Health Issues")
-    lung_problems = st.checkbox("Have you had any previous lung problems, such as asthma, bronchitis, or pneumonia?")
-    other_health_conditions = st.text_area("Do you have any other health conditions, especially heart disease or hypertension?")
-
-    # Medication and Treatment History
-    st.header("Medication and Treatment History")
-    current_medications = st.text_area("Are you currently taking any medications, including inhalers or other treatments for breathing problems?")
-    treatments_surgeries = st.text_area("Have you undergone any treatments or surgeries that might affect your lungs?")
-
-    # Symptom Onset and Progression
-    st.header("Symptom Onset and Progression")
-    symptom_onset_date = st.date_input("When did you first notice your symptoms?")
-    symptom_progression = st.checkbox("Have your symptoms been getting worse over time?")
-
-    # Allergies
-    st.header("Allergies")
-    respiratory_allergies = st.text_area("Do you have any allergies that might affect your respiratory system?")
+    # ... (remaining code)
 
     # Save Survey Response
     if st.button("Submit Survey"):
