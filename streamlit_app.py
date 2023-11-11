fbd258c5a6d6e5b746a03d4097a0db8d785019ac
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 09:12:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,16 @@ import streamlit as st
 def welcome_screen():
     st.title("Welcome to Lung Assist Respiratory Survey")
     st.write("This survey is designed to gather information about your respiratory health.")
+
+    if "next_button_clicked" not in st.session_state:
+        st.session_state.next_button_clicked = False
+
     next_button = st.button("Next")
-    st.empty()
 
     if next_button:
+        st.session_state.next_button_clicked = True
+
+    if st.session_state.next_button_clicked:
         st.empty()  # Clear the entire screen
         survey_page()
 
@@ -16,10 +22,9 @@ def survey_page():
     # Your survey questions and form go here...
 
     if st.button("Submit Survey"):
-        st.success("Your respiratory survey has been successfully submitted. Thank you!")
+        st.session_state.next_button_clicked = False  # Reset the button state
         st.empty()  # Clear the entire screen
-        
-    #st.success("Your respiratory survey has been successfully submitted. Thank you!")
+        st.success("Your respiratory survey has been successfully submitted. Thank you!")
 
 def main():
     welcome_screen()
