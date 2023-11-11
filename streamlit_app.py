c820b329ad4aa9eab4cbcafed52d92e223f55e24
Pavuluri-Nagabhargav <103000968+Pavuluri-Nagabhargav@users.noreply.github.com>
2023-11-11 02:47:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,80 +89,83 @@ def main():
     full_name = st.text_input("Full Name:")
     phone_number = st.text_input("Phone Number:")
 
-
-
     # Symptom-Related Questions
     st.header("Symptom-Related Questions")
-shortness_of_breath = st.checkbox("Have you been experiencing shortness of breath?")
-if shortness_of_breath:
-    physical_activity = st.checkbox("Does it occur or worsen with physical activity?")
+    shortness_of_breath = st.checkbox("Have you been experiencing shortness of breath?")
+    physical_activity = False  # Initialize to False
+    if shortness_of_breath:
+        physical_activity = st.checkbox("Does it occur or worsen with physical activity?")
 
-persistent_cough = st.checkbox("Do you have a persistent cough?")
-if persistent_cough:
-    cough_type = st.radio("Is it dry or productive (producing mucus)?", ["Dry", "Productive"])
-    if cough_type == "Productive":
-        mucus_production = st.checkbox("Have you noticed an increase in mucus production?")
-        if mucus_production:
-            mucus_color = st.text_input("What is the color of the mucus?")
-            mucus_consistency = st.text_input("What is the consistency of the mucus?")
+    persistent_cough = st.checkbox("Do you have a persistent cough?")
+    cough_type = ""  # Initialize to an empty string
+    mucus_production = False  # Initialize to False
+    mucus_color = ""  # Initialize to an empty string
+    mucus_consistency = ""  # Initialize to an empty string
+    if persistent_cough:
+        cough_type = st.radio("Is it dry or productive (producing mucus)?", ["Dry", "Productive"])
+        if cough_type == "Productive":
+            mucus_production = st.checkbox("Have you noticed an increase in mucus production?")
+            if mucus_production:
+                mucus_color = st.text_input("What is the color of the mucus?")
+                mucus_consistency = st.text_input("What is the consistency of the mucus?")
 
-respiratory_infections = st.checkbox("Are you experiencing frequent respiratory infections?")
-chest_symptoms = st.checkbox("Do you have wheezing or chest tightness?")
+    respiratory_infections = st.checkbox("Are you experiencing frequent respiratory infections?")
+    chest_symptoms = st.checkbox("Do you have wheezing or chest tightness?")
 
-# History of Smoking
-st.header("History of Smoking")
-smoked_before = st.checkbox("Have you ever smoked?")
-if smoked_before:
-    smoking_duration = st.number_input("For how long did you smoke? (in years)")
-    cigarettes_per_day = st.number_input("How many cigarettes per day did you smoke?")
-    former_smoker = st.checkbox("Are you a former smoker?")
-    if former_smoker:
-        quit_date = st.date_input("When did you quit smoking?")
+    # History of Smoking
+    st.header("History of Smoking")
+    smoked_before = st.checkbox("Have you ever smoked?")
+    smoking_duration = 0  # Initialize to 0
+    cigarettes_per_day = 0  # Initialize to 0
+    former_smoker = False  # Initialize to False
+    quit_date = ""  # Initialize to an empty string
+    secondhand_smoke = False  # Initialize to False
+    if smoked_before:
+        smoking_duration = st.number_input("For how long did you smoke? (in years)")
+        cigarettes_per_day = st.number_input("How many cigarettes per day did you smoke?")
+        former_smoker = st.checkbox("Are you a former smoker?")
+        if former_smoker:
+            quit_date = st.date_input("When did you quit smoking?")
 
-    secondhand_smoke = st.checkbox("Are you exposed to secondhand smoke?")
+        secondhand_smoke = st.checkbox("Are you exposed to secondhand smoke?")
 
-# ... (remaining code)
+    # Occupational and Environmental Exposure
+    st.header("Occupational and Environmental Exposure")
+    workplace_exposure = st.checkbox("Are you exposed to dust, chemicals, fumes, or other irritants at your workplace?")
+    air_pollution = st.checkbox("Do you live in an area with high air pollution?")
 
-# Save Survey Response
-if st.button("Submit Survey"):
-    # Collect data from the UI
-    survey_data = {
-        "full_name": full_name,
-        "phone_number": phone_number,
-        "shortness_of_breath": shortness_of_breath,
-        "physical_activity": physical_activity,
-        "persistent_cough": persistent_cough,
-        "cough_type": cough_type,
-        "mucus_production": mucus_production,
-        "mucus_color": mucus_color,
-        "mucus_consistency": mucus_consistency,
-        "respiratory_infections": respiratory_infections,
-        "chest_symptoms": chest_symptoms,
-        "smoked_before": smoked_before,
-        "smoking_duration": smoking_duration,
-        "cigarettes_per_day": cigarettes_per_day,
-        "former_smoker": former_smoker,
-        "quit_date": quit_date,
-        "secondhand_smoke": secondhand_smoke,
-        "workplace_exposure": workplace_exposure,
-        "air_pollution": air_pollution,
-        "family_history": family_history,
-        "daily_activities": daily_activities,
-        "regular_activities": regular_activities,
-        "weight_loss_weakness": weight_loss_weakness,
-        "lung_problems": lung_problems,
-        "other_health_conditions": other_health_conditions,
-        "current_medications": current_medications,
-        "treatments_surgeries": treatments_surgeries,
-        "symptom_onset_date": symptom_onset_date,
-        "symptom_progression": symptom_progression,
-        "respiratory_allergies": respiratory_allergies
-    }
+    # Family History
+    st.header("Family History")
+    family_history = st.checkbox("Is there a history of COPD or other chronic respiratory diseases in your family?")
 
-    # Call the function to save the survey response
-    save_survey_response(survey_data)
-    
+    # Lifestyle and Daily Impact
+    st.header("Lifestyle and Daily Impact")
+    daily_activities = st.text_area("How do your symptoms affect your daily activities and quality of life?")
+    regular_activities = st.checkbox("Are you able to perform regular activities without becoming short of breath?")
+    weight_loss_weakness = st.checkbox("Have you noticed any weight loss or muscle weakness?")
+
+    # Previous Health Issues
+    st.header("Previous Health Issues")
+    lung_problems = st.checkbox("Have you had any previous lung problems, such as asthma, bronchitis, or pneumonia?")
+    other_health_conditions = st.text_area("Do you have any other health conditions, especially heart disease or hypertension?")
+
+    # Medication and Treatment History
+    st.header("Medication and Treatment History")
+    current_medications = st.text_area("Are you currently taking any medications, including inhalers or other treatments for breathing problems?")
+    treatments_surgeries = st.text_area("Have you undergone any treatments or surgeries that might affect your lungs?")
+
+    # Symptom Onset and Progression
+    st.header("Symptom Onset and Progression")
+    symptom_onset_date = st.date_input("When did you first notice your symptoms?")
+    symptom_progression = st.checkbox("Have your symptoms been getting worse over time?")
+
+    # Allergies
+    st.header("Allergies")
+    respiratory_allergies = st.text_area("Do you have any allergies that might affect your respiratory system?")
+
+    # Save Survey Response
+    if st.button("Submit Survey"):
+        response = save_survey_response(locals())
 
 if __name__ == "__main__":
     main()
-
